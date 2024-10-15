42a275ed23ec72d3c5e84382fbe0e2c299275941
Ginxss <ginxss@gmx.de>
2024-10-15 22:15:53 +0200

    expand tab

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -15,7 +15,7 @@ vim.opt.cursorline = true
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = false
+vim.opt.expandtab = true
 
 vim.opt.splitright = true
 vim.opt.splitbelow = true
