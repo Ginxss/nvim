ae27340dd3cb0953c02874a2446dba626f693af4
Ginxss <ginxss@gmx.de>
2024-11-22 18:15:21 +0100

    responsive toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,20 +2,48 @@ return {
 	"akinsho/toggleterm.nvim",
 	version = "*",
 	keys = { "<leader>t" },
-	opts = {
-		open_mapping = "<leader>t",
-		insert_mappings = false,
-		terminal_mappings = false,
-		persist_mode = false,
-		direction = "float",
-		float_opts = {
-			border = "double",
-			width = function()
-				return math.floor(vim.o.columns * 0.7)
-			end,
-			height = function()
-				return math.floor(vim.o.lines * 0.9)
-			end,
-		},
-	},
+	config = function()
+		local function wider()
+			return vim.o.columns > (vim.o.lines * 2)
+		end
+
+		require("toggleterm").setup({
+			open_mapping = "<leader>t",
+			insert_mappings = false,
+			terminal_mappings = false,
+			persist_mode = false,
+			shell = "fish",
+			direction = "float",
+			float_opts = {
+				border = "curved",
+				width = function()
+					if wider() then
+						return math.floor(vim.o.columns * 0.5)
+					else
+						return vim.o.columns
+					end
+				end,
+				height = function()
+					if wider() then
+						return vim.o.lines - 4 -- leave space for lualine at the bottom
+					else
+						return math.floor(vim.o.lines * 0.5) - 4
+					end
+				end,
+				row = function() -- avoid overlapping lualine -> can't just set to max like col
+					if wider() then
+						return 0
+					else
+						return math.ceil(vim.o.lines * 0.5)
+					end
+				end,
+				col = vim.o.columns, -- always as right as possible - floating window can't get outside of bounds
+			},
+			highlights = {
+				FloatBorder = {
+					link = "FloatBorder",
+				},
+			},
+		})
+	end,
 }
