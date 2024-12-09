7d496877e52135bdb7dc055076360d0581e08e44
Ginxss <ginxss@gmx.de>
2024-12-09 21:45:47 +0100

    telescope mappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,8 @@
+-- Default keymappings in insert mode:
+-- <C-C> close
+-- <C-V> open vertical
+-- <C-X> open horizontal
+
 return {
 	"nvim-telescope/telescope.nvim",
 	branch = "0.1.x",
@@ -6,11 +11,75 @@ return {
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	},
 	keys = {
-		-- Using require("telescope.builtin") here causes telescope to be loaded at startup.
-		-- Setting this as the lazy load event and initializing the keybindings in config is ok
-		-- because neovim/lazy.nvim finishes loading the plugin before reading the next input.
-		-- Tested with simulated immediate keypresses.
-		{ "<leader>f" },
+		{
+			"<leader>ff",
+			function()
+				require("telescope.builtin").find_files()
+			end,
+		},
+		{
+			"<leader>fo",
+			function()
+				require("telescope.builtin").oldfiles()
+			end,
+		},
+		{
+			"<leader>fw",
+			function()
+				require("telescope.builtin").live_grep()
+			end,
+		},
+		{
+			"<leader>fg",
+			function()
+				require("telescope.builtin").git_status()
+			end,
+		},
+		{
+			"<leader>fr",
+			function()
+				require("telescope.builtin").lsp_references()
+			end,
+		},
+		{
+			"<leader>fi",
+			function()
+				require("telescope.builtin").lsp_implementations()
+			end,
+		},
+		{
+			"<leader>fd",
+			function()
+				require("telescope.builtin").diagnostics()
+			end,
+		},
+		{
+			"<leader>fh",
+			function()
+				require("telescope.builtin").help_tags()
+			end,
+		},
+		{
+			"<leader>ft",
+			function()
+				require("telescope.builtin").treesitter()
+			end,
+		},
+		{
+			"<leader>fc",
+			function()
+				require("tokyonight")
+				require("telescope.builtin").colorscheme()
+			end,
+		},
+		{
+			"<leader>fn",
+			function()
+				require("telescope.builtin").find_files({
+					cwd = vim.fn.stdpath("config"),
+				})
+			end,
+		},
 	},
 	config = function()
 		local telescope = require("telescope")
@@ -25,20 +94,5 @@ return {
 			},
 		})
 		telescope.load_extension("fzf")
-
-		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>ff", builtin.find_files)
-		vim.keymap.set("n", "<leader>fo", builtin.oldfiles)
-		vim.keymap.set("n", "<leader>fw", builtin.live_grep)
-		vim.keymap.set("n", "<leader>fg", builtin.git_status)
-		vim.keymap.set("n", "<leader>fr", builtin.lsp_references)
-		vim.keymap.set("n", "<leader>fi", builtin.lsp_implementations)
-		vim.keymap.set("n", "<leader>fd", builtin.diagnostics)
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags)
-		vim.keymap.set("n", "<leader>ft", builtin.treesitter)
-		vim.keymap.set("n", "<leader>fc", function()
-			require("tokyonight")
-			builtin.colorscheme()
-		end)
 	end,
 }
